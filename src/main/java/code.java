558c19bfae43e072a13000ceba8c6b8c62edb3e0
Hrishikesh <78008167+Hrishikesh009@users.noreply.github.com>
2025-11-24 10:35:07 +0530

    Update code.java

--- src/main/java/code.java
+++ src/main/java/code.java
@@ -1,5 +1,5 @@
 public class code {
     public static void main(String[] args) {
-        System.out.println("Hello from Hrishi’s CI/CD Pipeline!");
+        System.out.println("Hello from Hrishi's CI/CD Pipeline!");
     }
 }
